ed9797fbc369d7a09e4b1bdcb8bcdd86d6dad497
wangyongtao <wangyt4dev@gmail.com>
2018-09-16 14:40:49 +0800

    first commit

---
+++ src/main/java/github/wyt/gittraning/GitTraningApplication.java
@@ -0,0 +1,12 @@
+package github.wyt.gittraning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTraningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTraningApplication.class, args);
+	}
+}
---
+++ src/test/java/github/wyt/gittraning/GitTraningApplicationTests.java
@@ -0,0 +1,16 @@
+package github.wyt.gittraning;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GitTraningApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
