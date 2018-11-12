f453564c2cb6e372260ebac06d06db14c90a851c
wangyongtao <wangyt4dev@gmail.com>
2018-11-12 14:28:35 +0800

    增加注释.

--- src/main/java/github/wyt/gittraning/GitTraningApplication.java
+++ src/main/java/github/wyt/gittraning/GitTraningApplication.java
@@ -3,6 +3,10 @@ package github.wyt.gittraning;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @Author wangyongtao
+ * @Date 2018/11/12
+ */
 @SpringBootApplication
 public class GitTraningApplication {
 
